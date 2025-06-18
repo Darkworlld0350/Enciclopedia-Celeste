8fe1dccfa8b33e607f2387060d94900b20133522
Victor Manuel Aranda <varanda31@gmail.com>
2025-06-18 11:53:55 -0600

    Creación de carpetas y Estructura

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Motor de plantillas
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rutas
+const mainRoutes = require('./routes/index');
+app.use('/', mainRoutes);
+
+// Servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
